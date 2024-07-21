6906463c4723fa4144d1c066375a18edbaee3d97
iLeoon <ahmedeihab709@yahoo.com>
2024-07-21 18:52:02 +0300

    (refactor): add e2e testing for authentication controller

--- src/sessions/session.config.ts
+++ src/sessions/session.config.ts
@@ -1,5 +1,5 @@
 import { ConnectMongoOptions } from 'connect-mongo/build/main/lib/MongoStore';
 export default {
   mongoUrl: process.env.MONGO_URI,
-  dbName: 'chat-app',
+  dbName: process.env.NODE_ENV === 'test' ? 'chat-app-test' : 'chat-app',
 } as ConnectMongoOptions;
